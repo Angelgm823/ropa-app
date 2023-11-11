9ed97fe1e84e6488b8fa356ec5057b10e8b21393
Angelgm <gomezmendezangel100@gmail.com>
2023-11-11 13:30:21 -0600

    Se actualiza vista de productos

--- app/Livewire/Category/CategoryShow.php
+++ app/Livewire/Category/CategoryShow.php
@@ -4,10 +4,12 @@ namespace App\Livewire\Category;
 
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use App\Models\Category;
 
 #[Title('Ver categoria')]
 class CategoryShow extends Component
 {
+    public Category $category;
     public function render()
     {
         return view('livewire.category.category-show');
